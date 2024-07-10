9bd2f1596ef23592e68d109b7a2cebc64a6ec901
harsha <c.harsha0909@gmail.com>
2024-07-10 10:18:22 +0530

    Added airport table with FK to city by adding new migration

--- src/models/airport.js
+++ src/models/airport.js
@@ -1,30 +1,45 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class City extends Model {
+  class Airport extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.hasMany(models.Airport, {
+      this.belongsTo(models.City, {
         foreignKey: "cityId",
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
       });
     }
   }
-  City.init(
+  Airport.init(
     {
       name: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
       },
+      code: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      address: {
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      cityId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
     },
     {
       sequelize,
-      modelName: "City",
+      modelName: "Airport",
     }
   );
-  return City;
+  return Airport;
 };
